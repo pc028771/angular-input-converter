53f59026d9ead068ffb5d0109997fc0b211b894a
Howard <howard.yang@kuobrothers.com>
2015-11-26 15:55:08 +0800

    initialize repo
    Signed-off-by: Howard <howard.yang@kuobrothers.com>

---
+++ input-converter.js
@@ -0,0 +1,46 @@
+angular.module('json.converter', [])
+.directive('input', ['$filter', function($filter) {
+	return {
+		restrict: 'AE',
+		require: '?ngModel',
+		compile: function(element, attrs){
+			return {
+				post: function(scope, element, attrs, ngModel){
+					if( !ngModel ) { return; }
+
+					switch( attrs.type ) {
+						case "date" :
+							ngModel.$formatters.push(function(value) {
+								return value ? new Date(value) : new Date();
+							});
+							ngModel.$parsers.push(function(value) {
+								return angular.isDate(value) ? $filter('date')(value, 'yyyy-MM-dd') : null;
+							});
+							break;
+						case "time" :
+							ngModel.$formatters.push(function(value) {
+								return value ? new Date('1970/01/01 ' + value) : new Date();
+							});
+							ngModel.$parsers.push(function(value) {
+								return angular.isDate(value) ? $filter('date')(value, 'HH:mm:ss') : null;
+							});
+							break;
+						case "datetime-local" :
+							ngModel.$formatters.push(function(value) {
+								return value ? new Date(value.replace(/-/g, '/')) : new Date();
+							});
+							ngModel.$parsers.push(function(value) {
+								return angular.isDate(value) ? $filter('date')(value, 'yyyy-MM-dd HH:mm:ss') : null;
+							});
+							break;
+						case "number":
+							ngModel.$formatters.push(function(value) {
+								return isNaN(value) ? 0 : parseInt(value);
+							});
+							break;
+					}
+				}
+			};
+		}
+	};
+}]);
